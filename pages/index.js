677334b474f4d4ec26ec57474561e9e0989ce823
SeanShade <Sean.shade@yahoo.com>
2025-05-30 15:47:55 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>🔮 Madam Shade Wolf's Tarot 🔮</h1>
+      <p>Your tarot reading site is working!</p>
+      <p>API Status: Ready for readings!</p>
+    </div>
+  );
+}
