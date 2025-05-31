730c546506ff96274c1c4118e5349d1cd5176614
SeanShade <Sean.shade@yahoo.com>
2025-05-31 01:34:17 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>🔮 Madam Shade Wolf's Tarot 🔮</h1>
-      <p>Your tarot reading site is working!</p>
-      <p>API Status: Ready for readings!</p>
-    </div>
-  );
-}
